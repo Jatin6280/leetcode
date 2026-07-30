d7520fc59b4b14a74c061f501b784ee56b9611be
Jatin <hasijajatin436@gmail.com>
2026-07-30 11:59:11 +0530

    Time: 4 ms (77.47%), Space: 128.2 MB (78.44%) - LeetHub - Time: O(n), Space: O(n)

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,35 +1,22 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+
         int n = nums.size();
-        
-        vector<int> first;  // Stores positive numbers
-        vector<int> second; // Stores negative numbers
-        vector<int> ans;    // Stores the final result
+        vector<int> ans(n);
 
-        // Step 1: Separate elements into positives and negatives
+        int pos = 0; // Even indices
+        int neg = 1; // Odd indices
+
         for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                first.push_back(nums[i]);
-            } else {
-                second.push_back(nums[i]);
+
+            if (nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos += 2;
             }
-        }
-
-        int idx1 = 0; // Tracks position in 'first'
-        int idx2 = 0; // Tracks position in 'second'
-
-        // Step 2: Interleave them back (Even index = positive, Odd index = negative)
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0 && idx1 < first.size()) {
-                ans.push_back(first[idx1++]);
-            } else if (idx2 < second.size()) {
-                ans.push_back(second[idx2++]);
+            else {
+                ans[neg] = nums[i];
+                neg += 2;
             }
         }
 
