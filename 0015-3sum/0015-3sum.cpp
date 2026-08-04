ceb12795e879f864431e9eaa95056f49cee26b9f
Jatin <hasijajatin436@gmail.com>
2026-08-05 00:42:37 +0530

    Time: 55 ms (27.75%), Space: 28.9 MB (92.73%) - LeetHub - Time: O(n^2), Space: O(n)

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+        if (n < 3) return result;
+
+        // Sort the array to use two pointers and skip duplicates
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+
+                    // Skip duplicates for the second and third numbers
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } 
+                else if (sum < 0) {
+                    left++; // Sum is too small, move left pointer up
+                } 
+                else {
+                    right--; // Sum is too large, move right pointer down
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
