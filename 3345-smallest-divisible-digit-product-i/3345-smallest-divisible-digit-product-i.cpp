9663f6b1581bc7fde86bcc9c6b63103bb3be7637
Jatin <hasijajatin436@gmail.com>
2026-08-06 12:04:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (5.46%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int smallestNumber(int n, int t) {
+
+        for (int i = n; i <= 100; i++) {
+
+            int x = i;
+            int product = 1;
+
+            while (x > 0) {
+                product *= (x % 10);
+                x /= 10;
+            }
+
+            if (product % t == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
