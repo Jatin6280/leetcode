5866a3cd1d8d632fac5daae202e6f9eff76a2051
Jatin <hasijajatin436@gmail.com>
2026-08-30 10:24:44 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.24%) - LeetHub - Time: O(log n), Space: O(1)

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        auto it = lower_bound(nums.begin(), nums.end(), target);
+        int n=nums.size();
+        int st =0;
+        int end=n-1;
         
-        // Check if the iterator is within bounds and actually points to the target
-        if (it != nums.end() && *it == target) {
-            return distance(nums.begin(), it);
+        while(st<=end){
+            long long mid=(end+st)/2;
+            if(nums[mid]>target){
+                end=mid-1;
+            }
+            else if (nums[mid]<target){
+                st =mid+1;
+            }
+            else{
+                return mid;
+            }
         }
-        
         return -1;
     }
 };
