fe1df00b0f89744780a2f90077e850fb28048146
Jatin <hasijajatin436@gmail.com>
2026-08-31 11:18:43 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.65%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        int st = 0;
+        int end = n - 1;
+
+        while(st <= end) {
+            int mid = st + (end - st) / 2;
+
+            if(target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if(target > nums[mid]) {
+                st = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+
+        return st;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
